4c7c28216b7c4e1e909fa6348feda5b70e3f85a2
CaspD3V <ihackz9v@gmail.com>
2021-07-09 23:51:17 -0400

    Fixed bugs and added shit

--- ScreenSaver/ContentView.swift
+++ ScreenSaver/ContentView.swift
@@ -10,9 +10,9 @@ import Combine
 import AVKit
 import AVFoundation
 
-final class WelcomeVideoController : UIViewControllerRepresentable {
+final class teighteen : UIViewControllerRepresentable {
     var playerLooper: AVPlayerLooper?
-    func makeUIViewController(context: UIViewControllerRepresentableContext<WelcomeVideoController>) ->
+    func makeUIViewController(context: UIViewControllerRepresentableContext<teighteen>) ->
         AVPlayerViewController {
         try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
         try? AVAudioSession.sharedInstance().setActive(true)
@@ -20,7 +20,7 @@ final class WelcomeVideoController : UIViewControllerRepresentable {
             controller.showsPlaybackControls = false
             
         guard let path = Bundle.main.path(forResource: "screen1", ofType:"m4v") else {
-                debugPrint("screen1.mp4 not found")
+                debugPrint("screen1.m4v not found")
                 return controller
             }
                     
@@ -35,13 +35,13 @@ final class WelcomeVideoController : UIViewControllerRepresentable {
             return controller
         }
     
-    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<WelcomeVideoController>) {
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<teighteen>) {
     }
 }
 
-final class WelcomeVideoController1 : UIViewControllerRepresentable {
+final class tnineteen : UIViewControllerRepresentable {
     var playerLooper: AVPlayerLooper?
-    func makeUIViewController(context: UIViewControllerRepresentableContext<WelcomeVideoController1>) ->
+    func makeUIViewController(context: UIViewControllerRepresentableContext<tnineteen>) ->
         AVPlayerViewController {
         try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
         try? AVAudioSession.sharedInstance().setActive(true)
@@ -49,7 +49,7 @@ final class WelcomeVideoController1 : UIViewControllerRepresentable {
             controller.showsPlaybackControls = false
             
         guard let path = Bundle.main.path(forResource: "screen2", ofType:"m4v") else {
-                debugPrint("screen1.mp4 not found")
+                debugPrint("screen2.m4v not found")
                 return controller
             }
                     
@@ -64,52 +64,112 @@ final class WelcomeVideoController1 : UIViewControllerRepresentable {
             return controller
         }
     
-    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<WelcomeVideoController1>) {
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<tnineteen>) {
     }
 }
-class BatteryModel : ObservableObject {
-    @Published var level = UIDevice.current.batteryLevel
-    private var cancellableSet: Set<AnyCancellable> = []
 
-    init () {
-        UIDevice.current.isBatteryMonitoringEnabled = true
-        assignLevelPublisher()
-    }
-
-    private func assignLevelPublisher() {
-        _ = UIDevice.current
-            .publisher(for: \.batteryLevel)
-            .assign(to: \.level, on: self)
-            .store(in: &self.cancellableSet)
+final class Earth : UIViewControllerRepresentable {
+    var playerLooper: AVPlayerLooper?
+    func makeUIViewController(context: UIViewControllerRepresentableContext<Earth>) ->
+        AVPlayerViewController {
+        try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
+        try? AVAudioSession.sharedInstance().setActive(true)
+            let controller = AVPlayerViewController()
+            controller.showsPlaybackControls = false
+            
+        guard let path = Bundle.main.path(forResource: "earth", ofType:"mp4") else {
+                debugPrint("earth.mp4 not found")
+                return controller
+            }
+                    
+            let asset = AVAsset(url: URL(fileURLWithPath: path))
+            let playerItem = AVPlayerItem(asset: asset)
+            let queuePlayer = AVQueuePlayer()
+            
+            playerLooper = AVPlayerLooper(player: queuePlayer, templateItem: playerItem)
+            queuePlayer.play()
+            controller.player = queuePlayer
+                
+            return controller
+        }
+    
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<Earth>) {
     }
 }
+
+
+//Battery % Breaks App When Plugged In
+//class BatteryModel : ObservableObject {
+   // @Published var level = UIDevice.current.batteryLevel
+   // private var cancellableSet: Set<AnyCancellable> = []
+
+    //init () {
+      //  UIDevice.current.isBatteryMonitoringEnabled = true
+       // assignLevelPublisher()
+//}
+
+    // private func assignLevelPublisher() {
+       // _ = UIDevice.current
+       //     .publisher(for: \.batteryLevel)
+       //     .assign(to: \.level, on: self)
+       //     .store(in: &self.cancellableSet)
+   // }
+//}
+
 struct ContentView: View {
     
-
-    @ObservedObject var batteryModel = BatteryModel()
+   //@ObservedObject var batteryModel = BatteryModel()
+    @State var screens = 0
     
-    @State private var showingGame = true
-    
+    func screenView() -> AnyView {
+         switch screens {
+         case 0: return AnyView(tnineteen())
+         case 1: return AnyView(teighteen())
+         case 2: return AnyView(Earth())
+        
+         default:
+            return AnyView(tnineteen())
+         }
+    }
+    //struct VisualEffectView: UIViewRepresentable {
+      //  var effect: UIVisualEffect?
+        //func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+        //func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
+    //}
     
     var body: some View {
         
         ZStack {
             
-            if showingGame {
-                WelcomeVideoController1().frame(idealWidth:230,idealHeight:500).aspectRatio(contentMode: .fill)
-                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                } else {
-                    WelcomeVideoController().frame(idealWidth:230,idealHeight:500).aspectRatio(contentMode: .fill)
-                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                }
+            screenView().frame(idealWidth:230,idealHeight:500).aspectRatio(contentMode: .fill)
+               .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             
+            
+            //VStack {
+                //HStack{
+                    //ZStack{
+                        //VisualEffectView(effect: UIBlurEffect(style: .dark)).frame(width:170,height:40).cornerRadius(30)
+                    //Text("\(Int(round(batteryModel.level * 100)))%")
+                        //.font(.headline)
+                        //.fontWeight(.heavy)
+                        //.foregroundColor(Color.green).frame(width:170,height:40).cornerRadius(30)
+                        
+                    
+                //}.padding(40.0)
+                //}
+               // Spacer()
+            //}
             
             VStack {
                 
                 HStack {
                     Spacer()
                     Button(action: {
-                        showingGame.toggle()
+                        if screens < 2{
+                        self.screens += 1
+                        } else {
+                            self.screens -= 2
+                        }
                     }
 ) {
                         Image("settings").resizable().frame(minWidth:0,maxWidth:.infinity,minHeight:0,maxHeight: .infinity).padding(30.0).opacity(0.0)
@@ -117,14 +177,7 @@ struct ContentView: View {
                 }
                 Spacer()
             }
-            VStack {
-                HStack{
-                    Text("Battery: \(Int(round(batteryModel.level * 100)))%")
-                        .padding(40.0)
-                    
-                }
-                Spacer()
-            }
+            
         }
     }
 }
